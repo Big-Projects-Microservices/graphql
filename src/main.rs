55a21fa0dafb8e94f75a00767608972bb68027ea
Bingine <alex.10.03.2006@gmail.com>
2025-04-12 15:39:19 +0200

    Added the user creation mutation (now for test purposes)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,68 @@
-use std::error::Error;
+use std::{env, error::Error, sync::Arc};
 
-use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
+use bson::{doc, to_vec};
+
+use async_graphql::{
+    Context, EmptySubscription, InputObject, Object, Schema, http::GraphiQLSource,
+};
 use async_graphql_poem::*;
 use poem::{listener::TcpListener, web::Html, *};
 
+use lapin::{
+    BasicProperties, Channel, Connection, ConnectionProperties, options::*, types::FieldTable,
+};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(InputObject)]
+struct NewUser {
+    username: String,
+    email: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct NewUserMessage {
+    username: String,
+    email: String,
+}
+
 struct Query;
+struct Mutation;
 
 #[Object]
 impl Query {
     async fn howdy(&self) -> &'static str {
         "partner"
+    }
+}
+
+#[Object]
+impl Mutation {
+    async fn create_user(&self, context: &Context<'_>, input: NewUser) -> bool {
+        let channel = context.data_unchecked::<Arc<Channel>>();
+
+        let message = NewUserMessage {
+            username: input.username.clone(),
+            email: input.email.clone(),
+        };
+
+        let serialized = bson::to_document(&message).expect("Failed to serialize");
+        let payload = to_vec(&serialized).expect("Failed to serialize");
+
+        channel
+            .basic_publish(
+                "user_exchange",
+                "user.new",
+                BasicPublishOptions::default(),
+                &payload,
+                BasicProperties::default(),
+            )
+            .await
+            .expect("Failed to publish")
+            .await
+            .expect("Failed to confirm");
+
+        true
     }
 }
 
@@ -20,8 +73,23 @@ async fn graphiql() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let address =
+        env::var("RABBITMQ_URL").unwrap_or_else(|_| "amqp://guest:guest@127.0.0.1:5672/%2f".into());
+
+    let connection = Connection::connect(&address, ConnectionProperties::default()).await?;
+    let channel = connection.create_channel().await?;
+
+    channel
+        .exchange_declare(
+            "user_exchange",
+            lapin::ExchangeKind::Direct,
+            ExchangeDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
     // create the schema
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    let schema = Schema::build(Query, Mutation, EmptySubscription).finish();
 
     // start the http server
     let app = Route::new().at("/", get(graphiql).post(GraphQL::new(schema)));
@@ -29,5 +97,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Server::new(TcpListener::bind("0.0.0.0:8000"))
         .run(app)
         .await?;
+
     Ok(())
 }
